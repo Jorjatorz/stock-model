008758656c47baeee01060787d80a2e413a874a4
Jorge Sanchez <jorjatorz@gmail.com>
2019-01-05 13:39:49 +0100

    Improved stock_data_getter performance and implementation

--- blueprints/stock_viewer.py
+++ blueprints/stock_viewer.py
@@ -15,12 +15,17 @@ def get_symbol_daily():
     graph_format = request.args.get('format')
     compact = True if request.args.get('compact') == 'true' or request.args.get('compact') is None else False
     if symbol and graph_format:
-        data = get_daily(symbol, format='json', full= not compact)
+        data = get_daily(symbol)
         if data is not None:
             if graph_format == 'line':
+                dates = list(data.keys()) if not compact else list(data.keys())[:100]
+                values = [elem['4. close'] for elem in data.values()]
+                if compact:
+                    values = values[:100]
+
                 trace = plotly.graph_objs.Scatter(
-                    x = list(data.keys()),
-                    y = [elem['4. close'] for elem in data.values()],
+                    x = dates,
+                    y = values,
                     mode = 'lines'
                 )
             return Response(json.dumps([trace], cls=plotly.utils.PlotlyJSONEncoder), mimetype='application/json')
--- model/stock_data_getter/stock_data_getter.py
+++ model/stock_data_getter/stock_data_getter.py
@@ -1,23 +1,74 @@
+import os
+import json
+from datetime import datetime
+import threading
+
 from alpha_vantage.timeseries import TimeSeries
 
 # TODO Put the API key inside config file
 API_KEY = 'WVACBBNC78SCCTUB'
 
 
-def get_daily(symbol, full = False, format = 'pandas'):
+def get_daily(symbol):
     """
     Returns the daily stock info of a symbol
     
     Arguments:
         symbol {str} -- Symbol of the stock
-    
-    Keyword Arguments:
-        full {bool} -- If True returns the full stock history. If not, only the last 100 elements (default: {False})
     """
     try:
-        print(full)
-        data, _ = TimeSeries(key=API_KEY, output_format=format).get_daily(symbol=symbol, outputsize='full' if full else 'compact')
+        data = _load_daily_data(symbol)
     except:
         return None
 
     return data
+
+
+def _load_daily_data(symbol):
+
+    file_name = "{}_daily.json".format(symbol)
+    file_path = "stock_data/{}".format(file_name)
+    # Check if we already have the symbol information
+    # TODO Make a special folder/path for the files
+    if os.path.isfile(file_path):
+        # File exists, load it and check if it's up to date
+        with open(file_path) as json_file:
+            symbol_json = json.load(json_file)
+
+        update_date = symbol_json['update_date']
+        update_date = datetime.strptime(update_date, '%d-%m-%Y')
+        today = datetime.now()
+        today = datetime(today.year, today.month, today.day)
+        if update_date < today:
+            print("{} - Updating {} file".format(__name__, file_name))
+            symbol_json = _update_symbol_file(symbol)
+            data = symbol_json['data']
+        else:
+            print("{} - Retrieving data from {} file".format(__name__, file_name))
+            data = symbol_json['data']
+    else:
+        print("{} - Creating {} file".format(__name__, file_name))
+        symbol_json = _update_symbol_file(symbol)
+        data = symbol_json['data']
+
+    return data
+
+def _update_symbol_file(symbol):
+        data, _ = TimeSeries(key=API_KEY, output_format='json').get_daily(symbol=symbol, outputsize='full')
+
+        thread = threading.Thread(target = _update_symbol_file_aux, args=(symbol, data))
+        thread.start()
+
+        return data
+
+def _update_symbol_file_aux(symbol, data):
+    file_name = "{}_daily.json".format(symbol)
+    file_path = "stock_data/{}".format(file_name)
+
+    symbol_data = {
+        'update_date': datetime.now().strftime('%d-%m-%Y'),
+        'data': data
+    }
+
+    with open(file_path, 'w') as json_file:
+        json.dump(symbol_data, json_file)
